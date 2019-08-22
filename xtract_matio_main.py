b0e33efafc7b5bd3aa14000567d9f6ec89508137
Ryan Wong <rewong03@gmail.com>
2019-08-22 16:27:52 -0500

    Fixed xtract_matio_main main function so it returns in the format we want

--- xtract_matio_main.py
+++ xtract_matio_main.py
@@ -1,23 +1,25 @@
 import time
 import argparse
-from materials_io.utils.interface import run_all_parsers, get_available_parsers
+from materials_io.utils.interface import run_all_parsers
 
 
-def args_to_parser(path):
+def extract_matio(path):
     """Runs a file through all available MaterialsIO parsers.
-
     Parameter:
     path (str): File path of file to parse.
-
     Return:
     meta_dictionary (dict): Dictionary of all metadata extracted using
     MaterialsIO parsers.
     """
+    t0 = time.time()
     meta_dictionary = {"matio": {}}
 
     parser_gen = run_all_parsers(path, exclude_parsers=['noop', 'generic', 'csv'])
     for parser_data in parser_gen:
         meta_dictionary["matio"].update({parser_data[1]: parser_data[2]})
+
+    total_time = time.time() - t0
+    meta_dictionary.update({"extract time": total_time})
 
     return meta_dictionary
 
@@ -28,8 +30,5 @@ if __name__ == "__main__":
                         type=str, required=True)
     args = parser.parse_args()
 
-    t0 = time.time()
     meta = args_to_parser(args.path)
-    meta.update({"extract time": time.time() - t0})
     print(meta)
-
