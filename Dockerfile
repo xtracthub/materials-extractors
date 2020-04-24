e5910399116bd25ffc5a2b098b97c8ae10aff23d
tskluzac <skluzacek@uchicago.edu>
2020-04-24 14:30:25 -0500

    updating to run_all_parsers_on_group

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,16 @@ FROM python:3.6
 
 MAINTAINER Globus Labs, University of Chicago (Ryan Wong rewong03@gmail.com; Tyler Skluzacek skluzacek@uchicago.edu) 
 
-RUN git clone https://github.com/materials-data-facility/MaterialsIO.git \
+RUN git clone -b match_adapter https://github.com/materials-data-facility/MaterialsIO.git \
     && cd MaterialsIO && pip install -e . \
     && pip install -r requirements.txt \
     && cd
-    
 
-ENV container_version=5
+# Get the Xtract code here so we have access to exceptions. 
+RUN git clone https://github.com/xtracthub/xtracthub-service.git \ 
+    && cp xtracthub-service/exceptions.py /
 
+ENV container_version=14
 
 COPY xtract_matio_main.py / 
+COPY test_file.py /
--- xtract_matio_main.py
+++ xtract_matio_main.py
@@ -1,10 +1,10 @@
 import time
 import argparse
-from materials_io.utils.interface import execute_parser, get_available_parsers
+from materials_io.utils.interface import get_available_parsers, get_parser, run_all_parsers_on_group
 
 import os
 
-def extract_matio(path, parser):
+def extract_matio(paths, parser):
     """Runs a file through all available MaterialsIO parsers.
     Parameter:
     path (str): File path of file to parse.
@@ -13,31 +13,72 @@ def extract_matio(path, parser):
     MaterialsIO parsers.
     """
 
-    single_file_ls = ['crystal']
+    #single_file_ls = ['crystal', 'ase', 'image', 'csv', 'electron_microscopy', 'electron']
 
-    if parser in single_file_ls:
-        single_file = True
+    # if parser in single_file_ls:
+    #     single_file = True
+    # else:
+    #     single_file= False
 
     t0 = time.time()
-    meta_dictionary = {"matio": {}}
-    file_list = os.listdir(path)
+    meta_dictionary = {"matio": {}, "parser": parser}
+
+    # file_list = os.listdir(path)
+    # This only exists here to get rid of that dang 'hidden' Macbook file in every dir by default... 
+    # without_ds = []
+    # for f in file_list:
+    #     if not f.startswith('.DS'):
+    #         full_path = os.path.join(path, f)
+    #         without_ds.append(full_path)
     
     # If single, file then get metadata from each file in the directory (/group)
-    if single_file:
-        meta_dictionary["matio"]["files"] = []
-        for file_n in file_list:
-            try: 
-                parser_gen = execute_parser(parser, f"{path}/{file_n}")
-                meta_dictionary["matio"]["files"].append({parser : parser_gen})
+    # if single_file:
+    #if parser in single_file_ls: 
+        #path = paths[0]  # Not clean, but easy way of getting one file as string :) 
+    #    meta_dictionary["matio"]["files"] = []
+    #    # for path in paths:  # TODO: why are we iterating through something 1 item long?
 
-            except Exception as e: 
-                # print(e)  # TODO: Should probably record that it's unextractable. 
-                pass
+    #    #fact_list = []
+    #    #for filen in paths:
+    #    #    if os.path.isfile(filen):
+    #    #        fact_list.append(True)
+    #    #    else:
+    #    #        fact_list.append(False)
+
+    #    # return fact_list
+    #    try: 
+    #        # parser_gen = execute_parser(parser, path)  # TODO
+    #        gen_dump = []
+    #        # return paths
+    #        parser_gen = run_all_parsers_on_group(group=paths, adapter_map='match', include_parsers=['ase'])
+
+    #        for item in parser_gen:
+    #            gen_dump.append(item)
+
+    #        meta_dictionary["matio"]["files"].append({parser : gen_dump})
+
+    #    except Exception as e: 
+    #        pass
 
     # This is the much easier case. Can just read in the entire directory.
-    else: 
-        parser_gen = execute_parser(parser, path)
-        meta_dictionary["matio"][parser] = parser_gen
+    if 'a'=='a':
+        # return "We made it here!"
+        meta_dictionary['debug'] = paths
+
+        try:
+            gen_dump = []
+            # parser_gen = execute_parser(parser, paths) # List index error from this line.
+            parser_gen = run_all_parsers_on_group(group=paths, adapter_map='match', include_parsers=[parser])
+            
+            for item in parser_gen:
+                gen_dump.append(item)
+            
+            meta_dictionary["matio"] = gen_dump
+
+        except Exception as e: 
+            return e
+            meta_dictionary['error1'] = f"THE LIST ERROR WAS HERE: {e}"
+
 
     meta_dictionary["container_version"] = os.environ['container_version']
 
@@ -49,8 +90,12 @@ def extract_matio(path, parser):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', help='file path or file group to parse',
+    parser.add_argument('--paths', help='list of files to parse',
                         type=str, required=True)
+    parser.add_argument('--parser', type=str, required=True)
     args = parser.parse_args()
-    meta = extract_matio(args.path)
+    meta = extract_matio(args.paths, args.parser)
+    print(meta)
+    # for item in meta:
+    #    print(item)
 
