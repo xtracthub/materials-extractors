5792393243fd9afe7c0e48d1f242b16f3e4b5b3d
tskluzac <skluzacek@uchicago.edu>
2022-03-14 22:18:30 -0500

    updating just in case my instance blows up

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,16 @@
-FROM python:3.6
+FROM python:3.8
 
 MAINTAINER Globus Labs, University of Chicago (Ryan Wong rewong03@gmail.com; Tyler Skluzacek skluzacek@uchicago.edu) 
 
 RUN pip install --upgrade pip
 
-RUN git clone https://github.com/materials-data-facility/MaterialsIO.git \
-    && cd MaterialsIO && pip install -e . \
-    && cd
-    #&& pip install -r requirements.txt \
-    #&& cd
+#RUN git clone https://github.com/materials-data-facility/MaterialsIO.git \
+#    && cd MaterialsIO && pip install . \
+#    && cd
+#    # && pip install -r requirements.txt \
+#    #&& cd
 
+# RUN echo pwd
 COPY mdf-materialsio-adapters /mdf-materialsio-adapters
 RUN cd /mdf-materialsio-adapters && pip install -e . && cd
 
@@ -19,20 +20,28 @@ COPY data-schemas /data-schemas
 RUN git clone https://github.com/xtracthub/xtracthub-service.git \ 
     && cp xtracthub-service/exceptions.py /
 
-RUN pip install xtract_sdk==0.0.7a4
+RUN pip install xtract_sdk==0.0.7a11
 RUN pip install funcx
 RUN pip install funcx_endpoint
 
 
 ENV CONTAINER_VERSION=1.0
 
-COPY xtract_matio_main.py / 
+
 RUN pip install xtract-sdk
 
-RUN pip uninstall globus_sdk -y && pip install globus_sdk==2.0.1
+# RUN pip uninstall globus_sdk -y && pip install globus_sdk==2.0.1
 
 RUN pip install ase dfttopif python-magic sphinx_rtd_theme tableschema xmltodict
 RUN pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 
-COPY test_file.py /
+RUN pip install mdf_toolbox
 
+COPY test_file.py /
+# pip install hyperspy still has weird problems in execution...
+RUN pip install pycalphad
+COPY MaterialsIO /MaterialsIO
+RUN cd MaterialsIO && pip install .
+RUN pip install numpy==1.22.2 && pip install pymatgen 
+
+COPY xtract_matio_main.py /
--- xtract_matio_main.py
+++ xtract_matio_main.py
@@ -1,12 +1,16 @@
 
+import os
+import sys
 import time
 import argparse
-from materials_io.utils.interface import run_all_parsers_on_group
-
-import os
+from materials_io.utils.interface import run_all_parsers_on_group, get_available_parsers
 
 
-def extract_matio(paths, parser):
+
+def extract_matio(paths):
+
+    # CSV omitted because cannot pass in anything. 
+    parsers=['crystal', 'csv', 'ase', 'dft', 'image', 'tdb']
     """Runs a file through MaterialsIO parsers.
 
     Parameter:
@@ -17,36 +21,78 @@ def extract_matio(paths, parser):
     MaterialsIO parsers.
     """
 
+    # This helps us handle the Xtract-case. 
+    if type(paths) == str:
+        paths = [paths]
+
+    all_mdata = []
+
+    path_sizes = dict()
+    # for path in paths: 
+
+    #for parser in parsers:
     t0 = time.time()
-    meta_dictionary = {"matio": {}, "parser": parser}
+    # meta_dictionary = {"matio": {}, "parser": pars}
+    
+    for path in paths:
+        if not os.path.isfile(path):
+            return {'error': f'file {path} does not exist'}
+        
+
+    meta_dictionary = dict()
 
     meta_dictionary['debug'] = paths
 
-    try:
+    # print(get_available_parsers().keys())
+
+    # print(f"Parsers: {parsers}") 
+
+    for par in parsers:
+        # print(f"Now executing parser...{par}")
+        ta = time.time()
         gen_dump = []
         # parser_gen = execute_parser(parser, paths) # List index error from this line.
-        parser_gen = run_all_parsers_on_group(group=paths, adapter_map="match", include_parsers=[parser])
+        try:
+            if par == 'ase':
+                if os.path.getsize(path) <= 32:
+                    # continue
+                    gen_dump.append({'error': 'file too small to process', 'extract_time': time.time()-ta})
+                    meta_dictionary[par] = gen_dump 
+                    #meta_dictionary['extraction_time'] = time.time() - ta
+                    continue
 
-        for item in parser_gen:
-            gen_dump.append(item)
+            
+            parser_gen = run_all_parsers_on_group(group=paths, adapter_map="match", include_parsers=[par])
+            # print(parser_gen)
+            for item in parser_gen:
+                gen_dump.append({'mdata': item, 'extract_time': time.time() - ta})
+            # print(f"Item for {par}: {item}")
+        except Exception as e:
+            gen_dump.append({'error': 'Unable to execute parser on group', 'parser': par, 'extract_time': time.time()-ta})
+        
+        meta_dictionary[par] = gen_dump
 
-        meta_dictionary["matio"] = gen_dump
-
-    except Exception as e:
-        # meta_dictionary["matio"]
-        meta_dictionary['error1'] = f"THE LIST ERROR WAS HERE: {e}"
+    t1 = time.time()
+    meta_dictionary['extraction_time'] = t1-t0
 
     meta_dictionary["CONTAINER_VERSION"] = os.environ['CONTAINER_VERSION']
 
     total_time = time.time() - t0
-    meta_dictionary.update({"extract time": total_time})
+    meta_dictionary.update({"time": total_time})
 
     return meta_dictionary
 
 
-def execute_extractor(paths, parser):
+def execute_extractor(paths, debug=False):
 
-    mdata = extract_matio(paths, parser)
+    if not debug:
+        # Need this to shush pycalphad
+        f = open('/dev/null', 'w')
+        sys.stdout = f
+
+
+
+    mdata = extract_matio(paths)
     return mdata
 
 
@@ -54,9 +100,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--paths', nargs='+', help='list of files to parse',
                         type=str, required=True)
-    parser.add_argument('--parser', type=str, required=True)
+    parser.add_argument('--parser', type=str, required=False)
+    # print(get_available_parsers().keys())
     args = parser.parse_args()
-    meta = extract_matio(args.paths, args.parser)
+    meta = execute_extractor(args.paths, debug=False)
+
+    # sys.stdout = t
+
     print(meta)
-
-
